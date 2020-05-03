b642fc3e60b9ec5382cf952c762e826492248a80
Stuart Stone <stuartstone123@gmail.com>
2020-05-03 22:50:22 +0100

    grpc servicer added

---
+++ alarm/server.py
@@ -0,0 +1,48 @@
+import json
+from concurrent import futures
+from enum import Enum
+
+import pickledb
+import grpc
+from protos import alarm_pb2
+from protos import alarm_pb2_grpc
+
+
+class AlarmStoreServicer(alarm_pb2_grpc.AlarmStoreServicer):
+    """Provides Methods for implementing the alarm store service"""
+
+    def __init__(self, db_path):
+        self.db = pickledb.load(db_path, True)
+
+    def ListAlarms(self, request, context):
+        """Lists all alarms that are stored in the database"""
+        alarm_entries = self.db.getall()
+
+        response = alarm_pb2.ActionResponse()
+        for alarm_key, alarm_value in alarm_entries.items():
+            try:
+                alarm_entry = json.loads(alarm_value)
+                response.alarms.append(
+                    alarm_pb2.Alarm(
+                        id=alarm_key,
+                        day=alarm_entry['day'],
+                        time=alarm_entry['time']
+                    )
+                )
+            except json.JSONDecodeError:
+                print('couldnt load alarm')
+
+        response.status = 'SUCCESS'
+        return response
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    alarm_pb2_grpc.add_AlarmStoreServicer_to_server(
+        AlarmStoreServicer('test.db'), server
+    )
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
